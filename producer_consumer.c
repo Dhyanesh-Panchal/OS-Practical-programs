d6fbd01015527e793b256822cc3790dc8b143812
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-05-28 10:39:40 +0530

    PRoducer Consumer working properly with an integer Item.

--- producer_consumer.c
+++ producer_consumer.c
@@ -10,10 +10,14 @@ semaphore mutex = 1;
 semaphore full = 0;
 semaphore empty = bufferSize;
 
+/********** SEMAPHORE FUNCTIONS ***********/
 void up(semaphore *lock)
 {
+    //****No NEED==>// while((*lock)>=bufferSize); // This condition is for Full (when buffer is full, it will stop producer to produce)
+
     (*lock)++;
 }
+
 void down(semaphore *lock)
 {
     while ((*lock) <= 0)
@@ -21,46 +25,70 @@ void down(semaphore *lock)
     (*lock)--;
     // printf("Lock Acquired!");
 }
+/****************************************/
+
+/********** BUFFER FUNCTIONS ***********/
+void insertBuffer(int item, int *buffer)
+{
+    buffer[full] = item;
+}
+
+int getItemFromBuffer(int *buffer)
+{
+    int item = buffer[0];          // Get data
+    for (int i = 0; i < full; i++) // shift remaining Data
+    {
+        buffer[i] = buffer[i + 1];
+    }
+
+    return item;
+}
+
+void printBuffer(int *buffer)
+{
+    printf("\n\n The Buffer is: ");
+    for (int i = 0; i <= full; i++)
+    {
+        printf("%d, ", buffer[i]);
+    }
+}
+/*************************************/
 
 void *producer(void *buffer)
 {
     int item;
     while (1)
     {
-        // item = rand(); // generate a random item.
+        item = rand(); // generate a random item.
+        down(&empty);
         down(&mutex);
-        // down(&empty);
-        printf("\n\n\nPRODUCER ==> Lock Acquired!!!!");
-        for (int i = 0; i < 5; i++)
-        {
-            printf("\n\tProduce,Produce,Produce......!");
-            // sleep(1);
-        }
+        // printf("\n\n\nPRODUCER ==> Lock Acquired!!!!");
+        insertBuffer(item, (int *)buffer);
+        printf("\nPRODUCER ==> Inserted Item: %d  in buffer", item);
+        printBuffer(buffer);
+        sleep(1); // Just to make process visible
+        // printf("\nPRODUCER ==> Lock Release!!!!");
         up(&mutex);
-        printf("\nPRODUCER ==> Lock Release!!!!");
+        up(&full);
     }
 }
 
-void *consumer()
+void *consumer(void *buffer)
 {
     int item;
     while (1)
     {
+        down(&full);
         down(&mutex);
-        printf("\n\n\nCONSUMER ==> Lock Acquired!!!!");
-        for (int i = 0; i < 3; i++)
-        {
-            printf("\n\tConsume,Consume,Consume......!");
-            // sleep(1);
-        }
+        // printf("\n\n\nCONSUMER ==> Lock Acquired!!!!");
+        item = getItemFromBuffer((int *)buffer);
+        // printf("\nCONSUMER ==> Lock Release!!!!");
+        sleep(1); // Just to make process visible
         up(&mutex);
-        printf("\nCONSUMER ==> Lock Release!!!!");
-    }
-}
+        up(&empty);
 
-void printBuffer(int *buffer)
-{
-    printf("");
+        printf("\nCONSUMER ==> Consuming Item %d", item);
+    }
 }
 
 void main()
@@ -68,9 +96,11 @@ void main()
     int *buffer = (int *)calloc(bufferSize, sizeof(int));
 
     pthread_t producerThread, consumerThread;
-    pthread_create(&producerThread, NULL, producer, NULL);
-    pthread_create(&consumerThread, NULL, consumer, NULL);
+    pthread_create(&producerThread, NULL, producer, buffer);
+    pthread_create(&consumerThread, NULL, consumer, buffer);
 
     pthread_join(producerThread, NULL);
     pthread_join(consumerThread, NULL);
+
+    free(buffer);
 }
